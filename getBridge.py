4d78a0f1e22776925f2432dc92b1d3b608101c42
Nelson Hickman <HickmanN@tri-met.org>
2018-07-06 15:43:39 -0700

    Cleaned up code

--- getBridge.py
+++ getBridge.py
@@ -2,40 +2,42 @@
 # getBridge.py - Prints the status of the bridges.
 # Created by Nelson Hickman
 
-import json
 import requests
 
+# hardcode the API key
+secret = '?access_token=INSERT_API_KEY'
+
 # Download the JSON data from mulnomah county's API.
-url ='https://api.multco.us/bridges?access_token=YOUR-TOKEN-HERE
+url ='https://api.multco.us/bridges' + secret
 response = requests.get(url)
 response.raise_for_status()
 
 # Load JSON data into a Python variable.
-bridge_data = json.loads(response.text)
+bridge_data = response.json()
 
 #Loop through the bridges and print status
 
-for i in bridge_data:
-    if not i['isUp']:
-        print(i['name'] + ' is down')
-    elif i['isUp']:
-        print(i['name'] + ' is up')
+for bridge in bridge_data:
+    if not bridge['isUp']:
+        print(bridge['name'] + ' is down')
+    elif bridge['isUp']:
+        print(bridge['name'] + ' is up')
 
-# List of bridges that TriMet cares about
+# List of bridges that Org cares about
 bridge_list = ['hawthorne', 'morrison', 'burnside', 'broadway']
 
 # function to get next scheduled lift
 def get_bridge_schedule(bridge):
-    scheduleUrl = 'https://api.multco.us/bridges/%s/events/scheduled?access_token=YOUR-TOKEN-HERE' %(bridge)
+    scheduleUrl = f'https://api.multco.us/bridges/{bridge}/events/scheduled{secret}'
     schedule_response = requests.get(scheduleUrl)
     schedule_response.raise_for_status()
 
-    schedule_bridge_data = json.loads(schedule_response.text)
+    schedule_bridge_data = schedule_response.json()
     if schedule_bridge_data != []:
-        print('The next scheduled lift for %s is: ' + schedule_bridge_data[0] %(bridge))
+        print(f'The next scheduled lift for {bridge} is: ' + schedule_bridge_data[0])
     else:
-        print('no secheduled lift for %s' %(bridge))
+        print(f'no secheduled lift for {bridge}.')
 
 # get next scheduled lift
-for i in bridge_list:
-    get_bridge_schedule(i)
+for bridge in bridge_list:
+    get_bridge_schedule(bridge)
